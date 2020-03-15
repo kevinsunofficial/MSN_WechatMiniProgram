0a4a3373e9140b6b770f64f5055f466e0bec1041
kevinsunofficial <kevin.sun0612@hotmail.com>
2020-03-15 13:01:52 -0400

    asd

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -23,6 +23,61 @@ App({
         traceUser: true,
       })
     }
+
+    // // version comparison (from wechat team)
+    // function compareVersion(v1, v2) {
+    //   v1 = v1.split('.')
+    //   v2 = v2.split('.')
+    //   var len = Math.max(v1.length, v2.length)
+    //   while (v1.length < len) {
+    //     v1.push('0')
+    //   }
+    //   while (v2.length < len) {
+    //     v2.push('0')
+    //   }
+    //   for (var i = 0; i < len; i++) {
+    //     var num1 = parseInt(v1[i])
+    //     var num2 = parseInt(v2[i])
+    //     if (num1 > num2) {
+    //       return 1
+    //     }else if (num1 < num2) {
+    //       return -1
+    //     }
+    //   }
+    //   return 0
+    // }
+    
+    // // update module
+    // wx.getSystemInfo({
+    //   success: res => {
+    //     this.globalData.SDKVersion = res.SDKVersion
+    //     console.log(res.SDKVersion)
+    //   },
+    // })
+
+    // const version = compareVersion(this.globalData.SDKVersion, '1.9.90')
+    // if (version >= 0) {
+    //   const updateManager = wx.getUpdateManager()
+    //   updateManager.onCheckForUpdate(
+    //     res => {
+    //       if (res.hasUpdate) {
+    //         updateManager.onUpdateReady(
+    //           res => {
+    //             wx.showModal({
+    //               title: "有新版本可用",
+    //               content: "有新版本可用，是否重启更新？",
+    //               success: res => {
+    //                 if (res.confirm) {
+    //                   updateManager.applyUpdate()
+    //                 }
+    //               }
+    //             })
+    //           }
+    //         )
+    //       }
+    //     }
+    //   )
+    // }
   },
 
   onShow: function() {
